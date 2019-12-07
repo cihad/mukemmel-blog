6f59ae07055ffc1f62529c59b0b4dcca0b947e03
Cihad Paksoy <cihadp@gmail.com>
2019-12-07 17:30:49 +0300

    external link hatasi giderildi

--- components/layout.js
+++ components/layout.js
@@ -13,16 +13,16 @@ export default function Layout(props) {
 		<div className="hero">
 			<h1 className="hero-title">Selman Kahya</h1>
 			<div className="hero-social-links">
-			<Link href="https://medium.com/@selmankahya">
+			<Link href="//medium.com/@selmankahya">
 				<a className="social-link">Medium</a>
 			</Link>
-			<Link href="https://www.twitter.com/selmankahyax">
+			<Link href="//www.twitter.com/selmankahyax">
 				<a className="social-link">Twitter</a>
 			</Link>
-			<Link href="https://www.linkedin.com/in/selmankahya">
+			<Link href="//www.linkedin.com/in/selmankahya">
 				<a className="social-link">LinkedIn</a>
 			</Link>
-			<Link href="https://www.instagram.com/selmankahyax/?hl=en">
+			<Link href="//www.instagram.com/selmankahyax/?hl=en">
 				<a className="social-link">Instagram</a>
 			</Link>
 			</div>
