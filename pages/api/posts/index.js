b41b754f14a0b8d4e1086bbc9597ffad54132c4d
Cihad Paksoy <cihadp@gmail.com>
2019-12-14 18:02:14 +0300

    REST uygunlugu icin API api/post endpointi api/posts olarak degistirildi.

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,4 +1,4 @@
-import { Post } from "../../models"
+import { Post } from "../../../models"
 
 export default async (req, res) => {
   const posts = await Post.findAll({ order: [['createdAt', 'DESC']] });
