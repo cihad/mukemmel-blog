2049ba4c7d8b46ea99d1b4744e0a8532d7bbc2a8
Cihad Paksoy <cihadp@gmail.com>
2020-01-26 21:49:44 +0300

    Blog yazisi icin bazi stiller

--- styles/article.js
+++ styles/article.js
@@ -1,3 +1,46 @@
 export default <style className={"editor"} jsx>{`
+	[data-block] {
+		margin: 0 100px;
+	}
 
+	[data-block=Header] {
+		margin-top: 2em;
+		margin-bottom: 1em;
+	}
+
+	[data-block=Image] {
+		margin: 2em 0;
+	}
+
+	[data-block=Image] figcaption {
+		margin-top: .5em;
+		text-align: center;
+		font-style: italic;
+	}
+
+	[data-block=Code] {
+		position: relative;
+	}
+
+	[data-block=Code] pre {
+		border-radius: 6px;
+	}
+
+	[data-block=Code]::before {
+		content: attr(data-language);
+		position: absolute;
+		right: 10px;
+		top: 10px;
+		border-radius: 3px;
+		background-color: #00000033;
+		color: gray;
+		z-index: 1;
+		padding: 0 1em;
+		font-size: .8em;
+		text-transform: uppercase;
+	}
+
+	[data-block=Quote] blockquote {
+		margin: 0 40px 0;
+	}
 `}</style>
