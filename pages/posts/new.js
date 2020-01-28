62da084e0b9f6e5b4d1c27899ea801acb3237b50
Cihad Paksoy <cihadp@gmail.com>
2020-01-29 00:32:48 +0300

    Yeni yazi icin post propu verildi

--- pages/posts/new.js
+++ pages/posts/new.js
@@ -5,6 +5,6 @@ import HeaderSm from "../../components/HeaderSm"
 export default () => (
 	<Layout header={<HeaderSm />}>
 		<h1 className="h2 border-bottom pb-3 mb-3">Yeni İçerik Formu</h1>
-		<PostForm></PostForm>
+		<PostForm post={{title: '', body: ''}}></PostForm>
 	</Layout>
 )
